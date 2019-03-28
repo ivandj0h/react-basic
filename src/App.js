ea461b2e4fd42e9b0b9ca145fdcf1eb7a0b0f6a2
ivandi1980 <ivandi.djoh@gmail.com>
2019-03-28 22:48:25 +0800

    modified app styles

--- src/App.js
+++ src/App.js
@@ -47,8 +47,7 @@ class App extends Component {
     <div>
       <Navbar />
       <div className="App">
-        <div>
-          <br />
+        <div className="mt">
         {
           this.state.items.map((item, idx) => (
             <button type="button"  className="btn btn-secondary" onClick={(e) => {
@@ -58,14 +57,14 @@ class App extends Component {
           ))
         }    
         
-        <section>
+        <section className="mt">
             <Login show={this.state.active===0}  changeHandler={ this.changeAction } ref={instance => this.child[0] = instance} />
             <Register show={this.state.active===1} changeHandler={ this.changeAction } ref={instance => this.child[1] = instance} />  
             <Contact show={this.state.active===2}  changeHandler={ this.changeAction } ref={instance => this.child[2] = instance} />            
         </section>
         <div>
-          <button className="btn btn-primary mr-sm-2">{this.state.action.cancel.text}</button> 
-          <button className="btn btn-danger">{this.state.action.submit.text}</button>
+          <button className="btn btn-secondary mr-sm-2">{this.state.action.cancel.text}</button> 
+          <button className="btn btn-primary">{this.state.action.submit.text}</button>
         </div>
       </div>
       </div>
